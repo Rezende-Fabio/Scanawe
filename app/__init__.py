082c580811bceb6f64f87f5fae97615e36fba8fd
Rezende-Fabio <rezendefabio337@gmail.com>
2022-09-18 15:43:06 -0300

    Add login com admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
-from app import app, render_template
+from app import app, render_template, request
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", nome="Isso deu certo")
+    return render_template("index.html")
+
+
+@app.route("/autenticar", methods=["POST"])
+def autenticar():
+    usuario = request.form['user']
+    senha = request.form["senha"]
+    if usuario == "admin" and senha == "admin":
+        return render_template("menu.html")
+    else:
+        return "Senha errada"
+
