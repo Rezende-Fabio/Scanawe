d254b04e7354f6102060811a9a1cd77949fe0b6d
Rezende-Fabio <rezendefabio337@gmail.com>
2022-09-23 11:53:36 -0300

    Add rascunho dos templates

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,43 @@ def autenticar():
         return render_template("menu.html")
     else:
         return render_template("index.html", aviso=True)
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/token")
+def token():
+    return render_template("token.html")
+
+
+@app.route("/lista-empresas")
+def listaEmpresas():
+    return render_template("listaEmpresas.html")
+
+
+@app.route("/cadastrar-empresa")
+def cadastrarEmpresa():
+    return render_template("cadastrarEmpresa.html")
+
+
+@app.route("/lista-relatorios")
+def listaRelatorios():
+    return render_template("listaRelatorios.html")
+
+
+@app.route("/cadastrar-relatorio")
+def cadastarRelatorio():
+    return render_template("cadastrarRelatorio.html")
+
+
+@app.route("/lista-questionarios")
+def listaQuestionarios():
+    return render_template("listaQuestionarios.html")
+
+
+@app.route("/cadastrar-questionario")
+def cadastarQuestionario():
+    return render_template("cadastrarQuestionario.html")
