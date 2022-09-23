b31fb3b312864c3f693dfbbddeee6c0e9b559e71
Rezende-Fabio <rezendefabio337@gmail.com>
2022-09-23 13:24:48 -0300

    Add modal form para esqueci a senha

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,19 @@ def autenticar():
     if usuario == "admin" and senha == "admin":
         return render_template("menu.html")
     else:
-        return render_template("index.html", aviso=True)
+        return render_template("index.html", aviso=1)
+
+@app.route("/esqueci-senha")
+def esqueciSenha():
+    return render_template("index.html", aviso=2)
+
+@app.route("/esqueciSenha", methods=["POST"])
+def esqueciSenhaa():
+    with open("teste.txt", "a+") as txt:
+        email = request.form["email"]
+        usuario = request.form["usuario"]
+        txt.write(f"{email}\n{usuario}")
+    return render_template("index.html")
 
 
 @app.route("/dashboard")
